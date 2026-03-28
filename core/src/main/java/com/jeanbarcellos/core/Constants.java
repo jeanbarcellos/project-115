1eb26ffa06c8da723f22d16f072e14e2a413b00d
Jean Barcellos <jeanbarcellos@hotmail.com>
2026-03-28 12:07:37 -0300

    core - ajustes

---
+++ core/src/main/java/com/jeanbarcellos/core/Constants.java
@@ -0,0 +1,13 @@
+package com.jeanbarcellos.core;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Constants {
+
+    public static final String CORRELATION_ID_KEY = "correlationId";
+
+    public static final String CORRELATION_ID_HEADER = "X-Correlation-Id";
+
+}
