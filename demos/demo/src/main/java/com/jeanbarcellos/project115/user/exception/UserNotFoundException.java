1aaa59dff6817077fba345960ebcf4a537216c3e
Jean Barcellos <jeanbarcellos@hotmail.com>
2026-05-30 18:55:09 -0300

    remocao dos demos

--- demos/demo/src/main/java/com/jeanbarcellos/project115/user/exception/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.jeanbarcellos.project115.user.exception;
-
-public class UserNotFoundException extends RuntimeException {
-
-    public UserNotFoundException(String string) {
-        super(string);
-    }
-
-}
